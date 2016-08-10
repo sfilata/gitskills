1da944ab5e15744bb85cf80f4f272bb92dda7d00
Ezio Auditore <shubaiqiao@126.com>
2016-08-10 11:02:41 +0800

    代码规范优化

--- realize.js
+++ realize.js
@@ -1,3 +1,8 @@
+/**
+ * @file
+ * 一个高考倒计时显示器，显示下一场高考当天9:30距现在的时间
+ * @author Shu Baiqiao(shubaiqiao@baijiahulian.com)
+ */
 window.onload = function () {
     showTime();
 };
@@ -7,33 +12,30 @@ window.onload = function () {
  * @return null
  */
 function showTime () {
-    var nowTime = new Date();//当前时间
+    var nowTime = new Date();
+    // 当前时间
     var nextTimeYear = nowTime.getFullYear();
-    var endTime = new Date(nextTimeYear + "/6/8,9:30:00");//结束时间
-    var leftTime = parseInt((
-            endTime.getTime() - nowTime.getTime()) / 1000); // 取得一共还剩多少秒
+    var endTime = new Date(nextTimeYear + '/6/8,9:30:00');
+    // 结束时间
+    var leftTime = +(endTime.getTime() - nowTime.getTime()) / 1000;
+    // 取得一共还剩多少秒
 
-    /**
-     * 检测是否已过今年高考日期
-     * @param  {} leftTime < 0 [已过今天高考日期]
-     * @return {[type]} [离下一次高考正确时间]
-     */
     if (leftTime < 0) {
         nextTimeYear++;
-        endTime = new Date(nextTimeYear + "/6/8,9:30:00");
-        leftTime = parseInt((
-                endTime.getTime() - nowTime.getTime()) / 1000);
+        endTime = new Date(nextTimeYear + '/6/8,9:30:00');
+        leftTime = +(endTime.getTime() - nowTime.getTime()) / 1000;
     }
+    // 若今年已过，则时间改为下一年高考时间，重新计算剩余时间
 
-    var d =  parseInt(leftTime / (24 * 60 * 60));
-    var h =  parseInt(leftTime / (60 * 60) % 24);
-    var m =  parseInt(leftTime / 60 % 60);
-    var s =  parseInt(leftTime % 60);
+    var d =  +(leftTime / (24 * 60 * 60));
+    var h =  +(leftTime / (60 * 60) % 24);
+    var m =  +(leftTime / 60 % 60);
+    var s =  +(leftTime % 60);
+    //计算倒计时的天，小时，分钟，秒
 
-    document.getElementById("show").innerHTML =
-    "距高考开场时间还有: <br />"+d + "天 " + h + " 小时 " + m + " 分 " + s + " 秒";
+    document.getElementById('show').innerHTML =
+    '距高考开场时间还有: <br />'+
+    d + '天 ' + h + ' 小时 ' + m + ' 分 ' + s + ' 秒';
 
-    setTimeout("showTime()", 500);
+    setTimeout('showTime()', 500);
 }
-
-
