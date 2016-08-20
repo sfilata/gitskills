686cddb22e18fe9da92f8df82ec618686c074dc2
Ezio Auditore <shubaiqiao@126.com>
2016-08-20 17:16:09 +0800

    一些修改

--- js/app.js
+++ js/app.js
@@ -1,22 +1,19 @@
-requirejs.config({
-    baseurl: 'js/lib',
-
-    paths: {
-        app: '../app'
-    }
-});
-
-// start the main app logic
-requirejs(['hquery', 'canvas', 'app/sub'],
-    function ($, canvas, sub) {
-
-});
-
-define({
-    color: 'black',
-    size: 'unsize'
-});
-
 define(function (require) {
-    var mod = require('./app');
+    var mod = require('./lib/jquery-3.1.0.min');
+
+    var test = function () {
+        $(document).ready(function() {
+            var $cr = $('#cr');
+            var cr = $cr[0];
+            $cr.click(function () {
+                if(cr.checked) {
+                    alert('感谢你的支持，可以进行下一步操作');
+                }
+            });
+        });
+    };
+
+    return function () {
+        test();
+    };
 });
