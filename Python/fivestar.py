360adb8ee3e266d6181d3c63050ab4f72a10522f
sfilata <shubaiqiao@126.com>
2018-01-19 15:57:34 +0800

    修改五角星的初始参数

--- Python/fivestar.py
+++ Python/fivestar.py
@@ -6,6 +6,10 @@ def main():
     distance = 60
     step = 60
     amount = 4
+    turtle.up()
+    turtle.backward(200)
+    turtle.down()
+    turtle.pensize(2)
     turtle.color('blue')
     for j in range(amount):
         for i in range(5):
