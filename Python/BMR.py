1bf8be1dae549fcf51db53aadd8411a35564f16a
sfilata <shubaiqiao@126.com>
2018-01-19 15:52:15 +0800

    修改BMR计算器

--- Python/BMR.py
+++ Python/BMR.py
@@ -1,3 +1,22 @@
+"""
+    author: sfilata
+    function: BMR calculator (Run always)
+    version: 1.0
+    date: 2018.1.19  
+"""
+
+
+def calculate(weight, height, age, sex):
+    if sex == "male":
+        result = 13.7 * float(weight) + 5.0 * float(height) - 6.8 * int(age) + 66
+    elif sex == "female":
+        result = 9.6 * float(weight) + 1.8 * float(height) - 4.7 * int(age) + 65
+    else:
+        print("please input the right sex")
+        return -1
+    return result
+
+
 def main():
     flag = True
     while flag:
@@ -9,18 +28,13 @@ def main():
         height = input("Please input your height(cm):")
         # age = 23
         age = input("Please input your age:")
-        if sex == "male":
-            result = 13.7 * float(weight) + 5.0 * float(height) - 6.8 * int(age) + 66
-        elif sex == "female":
-            result = 9.6 * float(weight) + 1.8 * float(height) - 4.7 * int(age) + 65
-        else:
-            print("please input the right sex")
-            return 0
-        print("Your BMR number is " + str(result))
-        conflag = input("Do you want to try again?(y/n)")
-        if conflag.lower() != 'y' and conflag.lower() != 'yes':
+        result = calculate(weight, height, age, sex)
+        if result != -1:
+            print("Your BMR number is " + str(result))
+        confirm = input("Do you want to try again?(y/n)")
+        if confirm.lower() != 'y' and confirm.lower() != 'yes':
             flag = False
-        print(conflag.lower())
+        print(confirm.lower())
 
 
 if __name__ == '__main__':
