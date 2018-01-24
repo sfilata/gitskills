5f145990dd04192221e46b8b5962150de5f8f4a8
sfilata <shubaiqiao@126.com>
2018-01-24 17:20:26 +0800

    完成完整版的52天存钱挑战

--- Python/save52.py
+++ Python/save52.py
@@ -3,19 +3,49 @@
     Author sfilata
     Date Jan 23rd 2018
 """
+import time
+import datetime
+# init the list of money saved every week
+# amount, step
+def initsaveMoney(amount, step):
+    saveMoney = []
+    init = step
+    for item in range(amount):
+        saveMoney.append(init)
+        init += step
+    return saveMoney
+
+# calculate the sum of the all the money
+def calculate(saveMoney):
+    sum = 0
+    for item in saveMoney:
+        sum += item
+    return sum
+
+# set the money of the specified week
+def setMoney(saveMoney, week, money):
+    saveMoney[week] = money
+    return saveMoney
+
+def calculateWeek(day):
+    temp = day - 1
+    week = (temp // 7) + 1
+    return week
 
 def main():
-    init = 10
-    saveMoney = []
-    sum = 0
-    for item in range(52):
-        saveMoney.append(init)
-        sum += init
-        init += 10
-    # print(saveMoney)
+    saveMoney = initsaveMoney(52, 10)
+    sum = calculate(saveMoney)
     print("The sum of money saved is " + str(float('%.2f' % sum)))
     week = input("please input the week you want to check the money when:")
     print("The money when week No." + week + " is " + str(float('%.2f' % saveMoney[int(week) - 1])))
+    # date = input("please input the date(like: 2018-01-20):")
+    # day = time.strptime(date, "%Y-%m-%d")
+    # print("The date is " + str(calculateWeek(day.tm_yday)) + "th week in current year!")
+    # print("The Money should saved in " + str(calculateWeek(day.tm_yday)) + "th week is " + str(float('%.2f' % saveMoney[calculateWeek(day.tm_yday) - 1])))
+    date = input("please input the date(like: 2018-01-20):")
+    myDatetime = datetime.datetime.strptime(date, "%Y-%m-%d")
+    print("The date is " + str(myDatetime.isocalendar()[1]) + "th week in current year!")
+    print("The Money should saved in " + str(myDatetime.isocalendar()[1]) + "th week is " + str(float('%.2f' % saveMoney[myDatetime.isocalendar()[1] - 1])))
 
 
 
