27681e5e85448cf993a19c3052a8a4e9cc6a60f5
sfilata <shubaiqiao@126.com>
2018-01-26 10:47:56 +0800

    提交最终版计算年中第几天

--- Python/countDay.py
+++ Python/countDay.py
@@ -4,6 +4,8 @@
     date: Jan 25th 2018
 """
 
+import datetime
+
 def isLeapYear(year):
     if (year % 4 == 0):
         if (year % 100 == 0):
@@ -18,10 +20,11 @@ def isLeapYear(year):
 
 
 def initYearDic(isLeapYear):
-    result = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    # result = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    result = {1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31}
     if (isLeapYear):
         # result = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-        result[1] = 29
+        result[2] = 29
     else:
         # result = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
         pass
@@ -30,19 +33,18 @@ def initYearDic(isLeapYear):
 def calculateDay(inputYear):
     MytupleList = inputYear.split("/")
     Mytuple = initYearDic(isLeapYear(int(MytupleList[0])))
-    print(Mytuple)
+    # print(Mytuple)
     sum = 0
     if (int(MytupleList[1]) > 1):
         for item in range(int(MytupleList[1]) - 1):
-            sum += Mytuple[item]
+            sum += Mytuple[item + 1]
     sum += int(MytupleList[2])
     return sum
 
 def main():
-    inputYear = input("please input the date you want to count day(like:2018/01/20):")
-    print("The date is the " + str(calculateDay(inputYear)) + "th day in this year!")
-    
-
+    # inputYear = input("please input the date you want to count day(like:2018/01/20):")
+    # print("The date is the " + str(calculateDay(inputYear)) + "th day in this year!")
+    print(datetime.date.timetuple(datetime.datetime(2014, 3, 1)).tm_yday)
 
 if __name__ == "__main__":
     main()
--- Python/save52.py
+++ Python/save52.py
@@ -37,6 +37,9 @@ def main():
     sum = calculate(saveMoney)
     print("The sum of money saved is " + str(float('%.2f' % sum)))
     week = input("please input the week you want to check the money when:")
+    if (int(week) > 52):
+        print("The week is out of the range")
+        return 
     print("The money when week No." + week + " is " + str(float('%.2f' % saveMoney[int(week) - 1])))
     # date = input("please input the date(like: 2018-01-20):")
     # day = time.strptime(date, "%Y-%m-%d")
@@ -44,6 +47,7 @@ def main():
     # print("The Money should saved in " + str(calculateWeek(day.tm_yday)) + "th week is " + str(float('%.2f' % saveMoney[calculateWeek(day.tm_yday) - 1])))
     date = input("please input the date(like: 2018-01-20):")
     myDatetime = datetime.datetime.strptime(date, "%Y-%m-%d")
+    print(myDatetime.isocalendar())
     print("The date is " + str(myDatetime.isocalendar()[1]) + "th week in current year!")
     print("The Money should saved in " + str(myDatetime.isocalendar()[1]) + "th week is " + str(float('%.2f' % saveMoney[myDatetime.isocalendar()[1] - 1])))
 
