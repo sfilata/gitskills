04aaec261694efcb8f61e18d7e1fe04e47a9548c
sfilata <shubaiqiao@126.com>
2018-08-13 17:08:31 +0800

    change the fuction

--- homework/1/realize.js
+++ homework/1/realize.js
@@ -1,28 +1,28 @@
 /**
  * @file
- * 一个高考倒计时显示器，显示下一场高考当天9:30距现在的时间
- * @author Shu Baiqiao(shubaiqiao@baijiahulian.com)
+ * 一个生日倒计时显示器，显示下一场高考当天9:30距现在的时间
+ * @author Shu Baiqiao(sfilata@hotmail.com)
  */
 window.onload = function () {
     showTime();
 };
 
 /**
- * 显示还有多长时间高考
+ * 显示还有多长时间到生日高考
  * @return null
  */
 function showTime () {
     var nowTime = new Date();
     // 当前时间
     var nextTimeYear = nowTime.getFullYear();
-    var endTime = new Date(nextTimeYear + '/6/8,9:30:00');
+    var endTime = new Date(nextTimeYear + '/1/9,00:00:00');
     // 结束时间
     var leftTime = +(endTime.getTime() - nowTime.getTime()) / 1000;
     // 取得一共还剩多少秒
 
     if (leftTime < 0) {
         nextTimeYear++;
-        endTime = new Date(nextTimeYear + '/6/8,9:30:00');
+        endTime = new Date(nextTimeYear + '/1/9,00:30:00');
         leftTime = +(endTime.getTime() - nowTime.getTime()) / 1000;
     }
     // 若今年已过，则时间改为下一年高考时间，重新计算剩余时间
